49bd8ba35d7b626b2cc862b5a1d63f205bebb91a
RankiiJ <rkjyq2004@gmail.com>
2024-04-05 13:03:37 +1100

    upload file

--- front_end/client/src/App.js
+++ front_end/client/src/App.js
@@ -1,25 +1,61 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react';
+import './App.css';
+import axios from "axios";
 
 function App() {
 
-  const [data, setData] = useState([{}])
+  const [ files, setFiles ] = useState(null);
+  const [ progress, setProgress ] = useState({ started: false, pc: 0 });
+  const [ msg, setMsg ] = useState(null);
+  
+  function handleUpload() {
+    if (!files) {
+      setMsg("No file selected");
+      return;
+    }
 
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
+    const fd = new FormData();
+    for (let i=0; i<files.length; i++) {
+      fd.append(`file${i+1}`, files[i]);
+    }
+
+    setMsg("Uploading...");
+    setProgress(prevState => {
+      return {...prevState, started: true}
+    })
+
+    axios.post('http://httpbin.org/post', fd, {
+      onUploadProgress: (progressEvent) => { setProgress(prevState => {
+        return { ...prevState, pc: progressEvent.progress*100 }
+      }) },
+      headers: {
+        "Custom-Header": "value",
       }
-    )
-  }, [])
+    })
+    .then(res => {
+      setMsg("Upload successful");
+      console.log(res.data);
+    })
+    .catch(err => {
+      setMsg("Upload failed");
+      console.error(err)
+    });
+  }
 
   return (
-    <div>
+    <div className="App">
+
+      <h1>My App</h1>
+
+      <input onChange={ (e) => { setFiles(e.target.files) } } type="file" multiple/>
+
+      <button onClick={ handleUpload }>Upload</button>
+
+      { progress.started && <progress max="100" value={progress.pc}></progress> }
+      { msg && <span>{msg}</span> }
 
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
